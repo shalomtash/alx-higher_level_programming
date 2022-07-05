a143e5e82b818df072b2923e035af98ff67fa478
shalomtash <shalomtash93@gmail.com>
2022-07-05 16:47:40 +0000

    function that returns an object fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list and save them to file"""
-import sys
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""Script continually adds arguments to a JSON string in a file
+    called add_item.json
+"""
 
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file_name = "add_item.json"
 if __name__ == "__main__":
     try:
-        my_list = load_from_json_file(file_name)
-    except FileNotFoundError:
-        my_list = []
-    for i, arg in enumerate(sys.argv):
-        if i > 0:
-            my_list.append(arg)
-    save_to_json_file(my_list, file_name)
+        json_list = load_from_json_file("add_item.json")
+    
+    except Exception:
+        json_list = []
+    
+    i = 1
+    while (i < len(argv)):
+        json_list.append(argv[i])
+        i += 1
+    save_to_json_file(json_list, "add_item.json")
