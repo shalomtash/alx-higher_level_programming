ba4e8a92d0eaebe4c9f7a96fa3da0d1ef283ae18
shalomtash <shalomtash93@gmail.com>
2022-07-05 17:18:06 +0000

    hot fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,7 +4,7 @@
 """
 
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+from 5-save_to_json_file.py import save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == "__main__":
